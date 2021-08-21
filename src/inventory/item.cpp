ff9471bbf69de0480366114945de3cebf1c438ec
Carmine Margiotta <car.margiotta@icloud.com>
2021-08-21 21:07:56 +0200

    When updating quantity of an item, if it reaches 0 it will be deleted

--- src/inventory/item.cpp
+++ src/inventory/item.cpp
@@ -49,10 +49,23 @@ void item::update_quantity(int delta)
 	inventory_entry::update_quantity(delta);
 
 	Transaction transaction(database);
-	Statement query(database, "UPDATE inventory SET quantity = ? WHERE owner = ? AND name = ?");
-	query.bind(1, quantity);
-	query.bind(2, owner);
-	query.bind(3, name);
-	query.exec(); 
+
+	if (quantity > 0)
+	{	
+		Statement query(database, "UPDATE inventory SET quantity = ? WHERE owner = ? AND name = ?");
+		query.bind(1, quantity);
+		query.bind(2, owner);
+		query.bind(3, name);
+		query.exec(); 	
+	}
+	else  
+	{
+		//Quantity reached 0, remove this item
+		Statement query(database, "DELETE FROM inventory WHERE owner = ? AND name = ?");
+		query.bind(1, owner);
+		query.bind(2, name);
+		query.exec(); 	
+	}
+
 	transaction.commit();
 }
