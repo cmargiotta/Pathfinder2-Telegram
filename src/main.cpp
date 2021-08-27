46ece44d362f83f0a23e21cc0bcd9b0b62473ed2
Carmine Margiotta <car.margiotta@icloud.com>
2021-08-27 19:06:06 +0200

    Temporary main

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "database/database.hpp"
+#include "inventory/item_database.hpp"
+#include "bot/bot.hpp"
+
+using namespace pathfinder2;
+using std::string;
+
+int main()
+{
+	auto db = init_database("test.db");
+	item_database::get_instance(&*db);
+	
+	bot b (*db);
+
+	return 0;
+}
