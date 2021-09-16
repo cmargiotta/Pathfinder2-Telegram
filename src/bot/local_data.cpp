448efdb95f5c883f0f917c03c072a7e97c780ad6
Carmine Margiotta <car.margiotta@icloud.com>
2021-09-16 21:41:09 +0200

    Corrected json paths

--- src/bot/local_data.cpp
+++ src/bot/local_data.cpp
@@ -5,7 +5,7 @@
 
 nlohmann::json& pathfinder2::get_commands()
 {
-	static std::string json = std::string("/usr/share/inventory_bot/localizaion_data/commands_") + getenv("PF2_INV_LANG") + ".json";
+	static std::string json = std::string("/usr/share/inventory_bot/localization_data/commands_") + getenv("PF2_INV_LANG") + ".json";
 	static nlohmann::json commands;
 	static bool init = false;
 
@@ -20,7 +20,7 @@ nlohmann::json& pathfinder2::get_commands()
 
 nlohmann::json& pathfinder2::get_messages()
 {
-	static std::string json = std::string("/usr/share/inventory_bot/localizaion_data/commands_") + getenv("PF2_INV_LANG") + ".json";
+	static std::string json = std::string("/usr/share/inventory_bot/localization_data/messages_") + getenv("PF2_INV_LANG") + ".json";
 	static nlohmann::json messages;
 	static bool init = false;
 
