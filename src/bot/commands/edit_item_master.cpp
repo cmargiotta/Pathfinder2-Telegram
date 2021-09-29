a8326dc8ee48f7951eb1611408df808fee054d65
Carmine Margiotta <666cmoe666@gmail.com>
2021-09-29 19:51:59 +0000

    Update src/bot/commands/edit_item_master.cpp

--- src/bot/commands/edit_item_master.cpp
+++ src/bot/commands/edit_item_master.cpp
@@ -15,6 +15,10 @@ void pathfinder2::_edit_item_master(TgBot::Bot& bot, TgBot::Message::Ptr message
 		throw std::runtime_error("authorization_error");
 	}
 
+	auto& item_list = character_->get_inventory().get_item_list();
+    auto keyboard = pathfinder2::create_keyboard(item_list.begin(), item_list.end());
+    pathfinder2::add_button_row(keyboard, get_command("cancel", message->from->languageCode));
+
 	character_->set_context(get_message("item_edit_request", message->from->languageCode));
-	bot.getApi().sendMessage(id, character_->get_context(), false, 0, pathfinder2::remove_keyboard);
+	bot.getApi().sendMessage(id, character_->get_context(), false, 0, keyboard);
 }
